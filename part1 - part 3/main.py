4a116c75bd0e67cf0782521b2007fc85180651bb
amir <4amirbayat@gmail.com>
2024-05-12 12:37:24 +0330

    part 1 to 3

---
+++ part1 - part 3/main.py
@@ -0,0 +1,86 @@
+# ------ User enter some items! formatting string! -------
+
+# myName = input()
+# print(f"Hi, this is {myName}")
+
+# ------ len function ------
+
+# my_full_name = "Amir Bayat"
+# print(len(my_full_name))
+
+
+# ------ casting ------
+
+# number = 10
+# text = "Amir"
+# text_number = "3.14"
+
+# print(str(number) + text)
+# print(number + float(text_number))
+
+
+# ------ boolean operators ------
+
+# print(True and False)
+# print(not True)
+
+# ------ conditions -------
+
+# name = input()
+# if (name == "Amir"):
+#     print("This is me")
+# elif (name == "Arash"):
+#     print("Your brother")
+# else:
+#     print("Unknown")
+
+
+# ------ for loops ------
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(10, 20, 2):
+#     print(i)
+
+# import random
+# for i in range(10):
+#     print(random.randint(1, 10))
+
+# ------ sys exit ------
+
+# import sys
+# while True:
+#     print("Type exit to exit")
+#     response = input()
+#     if (response == 'exit'):
+#         sys.exit()
+#     print(f"Entered: {response}")
+
+# ------- function -------
+
+# def my_function(name):
+#     print(f"name is {name}")
+
+
+# my_function("Amir")
+
+# ------ None value: when a funcion returns nothing! ------
+
+# ------ keyword argument ------
+
+# print("cat", "dog", "fish", sep=",")
+
+
+# ------- scope global and local ------
+
+# ------- exception handling -------
+def my_divide(a=1, b=1):
+    try:
+        print(a/b)
+    except ZeroDivisionError:
+        print("Can not divide by zero")
+
+
+my_divide(a=1, b=2)
+my_divide(a=1, b=0)
